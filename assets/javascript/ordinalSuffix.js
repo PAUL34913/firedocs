301ffc92a40733bf39da29cb0b7e782c5561c54d
Redbutcher <johndoe@example.compaul34913@gmail.com>
2024-07-11 00:15:19 +0100

    more javascript, goodnight

--- assets/javascript/ordinalSuffix.js
+++ assets/javascript/ordinalSuffix.js
@@ -1,3 +1,9 @@
+var isValidInput1 = false;
+var isValidInput2 = false;
+var input;
+var characterPosition;
+var nameLength;
+
 function getOrdinalSuffix(number) {
     if (number % 100 >= 11 && number % 100 <= 13) {
         return number + "th";
@@ -14,17 +20,42 @@ function getOrdinalSuffix(number) {
     }
 }
 
-var input = prompt("What is your first name?");
-var name = input;  // input is already a string
-var nameLength = input.length;
+while (!isValidInput1) {
+    input = prompt("What is your first name?");
+  
+    if (!input) {
+        alert("Please enter something.");
+    } else if (!isNaN(input)) {
+        alert("Numbers are not allowed.");
+    } else {
+        nameLength = input.length;
+        alert(input.length);
+        input = input.toLowerCase();
+        input = input.charAt(0).toUpperCase() + input.slice(1);
+        isValidInput1 = true;
+        alert(input);
+    }
+}
 
-var characterPosition = prompt("Hi " + name + ", your first name is " + nameLength + " characters long. Please pick a number from 1 to " + nameLength);
-characterPosition = parseInt(characterPosition);  // Convert the input to an integer
+while (!isValidInput2) {
+    characterPosition = prompt("Hi " + input + ", your first name is " + nameLength + " characters long. \n \nPlease pick a number from 1 to " + nameLength + ".");
+  
+    if (!characterPosition) {
+        alert("Please enter something.");
+    } else if (isNaN(characterPosition)) {
+        alert("Letters are not allowed.");
+    } else {
+        isValidInput2 = true;
+        characterPosition = parseInt(characterPosition);
+        alert(characterPosition);
+        alert(nameLength);
+    }
+}
 
 if (characterPosition >= 1 && characterPosition <= nameLength) {
     var ordinalSuffix = getOrdinalSuffix(characterPosition);
-    var pickedCharacter = name.charAt(characterPosition - 1);  // Get the character at the picked position
-    alert("You have picked the " + ordinalSuffix + " character which is '" + pickedCharacter + "'.");
+    var pickedCharacter = input.charAt(characterPosition - 1);  // Get the character at the picked position
+    alert("You have picked the " + ordinalSuffix + " character which is " + pickedCharacter + ".");
 } else {
     alert("Invalid number. Please pick a number from 1 to " + nameLength);
 }
